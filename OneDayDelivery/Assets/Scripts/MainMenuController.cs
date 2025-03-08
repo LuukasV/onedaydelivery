cba5bb441dc1e07d3e732648039f28a8638682bb
Luukas Vuolle <lvvuolle@student.jyo.fi>
2025-03-08 16:58:43 +0200

    Press esc to activate escape confirmation menu, you can go now between MaineMenu and MailBoxTestZone seemlessly.

--- OneDayDelivery/Assets/Scripts/MainMenuController.cs
+++ OneDayDelivery/Assets/Scripts/MainMenuController.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 //Includes functions for all main menu buttons
 public class MainMenuController : MonoBehaviour
 {
+    public GameObject popUpCanvas;
+
+    //The Main Menu puts the cursor visible (if it is not already)
+    public void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    //If Esc-button is pressed, we activate the pop-up canvas, which (hopefully) has the "Are you sure you want to Quit?" functionality
+    public void Update()
+    {
+        if(Input.GetKeyDown("escape"))
+        {
+            popUpCanvas.SetActive(true);
+        }
+
+    }
+
     //Ends the Game
     public void ButtonEffect_ConfirmQuit()
     {
