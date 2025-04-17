7195f8d277f60bd0e53667843040d8cef32a19fe
mianheha <milo.a.h.hankama@student.jyu.fi>
2025-04-17 15:47:51 +0300

    added PlayRandomSound script, added opening cutscene to level one

---
+++ OneDayDelivery/Assets/Scripts/PlayRandomSound.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+
+//Plays a random sound from a list of sound effects with the selected audiosource
+public class PlayRandomSound : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public AudioClip[] audioClips;
+
+    void OnTriggerEnter()
+    {
+        
+
+
+        //choose a random audioclip to play
+        AudioClip randomClip = audioClips[Random.Range(0, audioClips.Length)];
+        
+        //play the audioclip with audiosource
+        audioSource.PlayOneShot(randomClip);
+
+    }
+
+}
