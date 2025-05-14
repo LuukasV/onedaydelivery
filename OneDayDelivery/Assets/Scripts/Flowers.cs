d2f891917eff615f552cb7a986f3d7c557ed7861
Peketeus <p3keteus@gmail.com>
2025-05-14 13:08:02 +0300

    Fixed minefield boxcollider and added flower prefabs

---
+++ OneDayDelivery/Assets/Scripts/Flowers.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+// Player can stomp flowers and make them flat. Code replaces the original flower with the flat one
+public class Flowers : MonoBehaviour
+{
+    public GameObject stompedFlower;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Mailable")) // Checks if player or box hits the flower
+        {
+            StompFlower();
+        }
+    }
+
+    // Spawns the stomped flower with random rotation and destroys the original
+    private void StompFlower()
+    {
+        Quaternion randomRotation = Quaternion.Euler(0, Random.Range(0f, 360f), 0);
+        Instantiate(stompedFlower, transform.position, randomRotation);
+        Destroy(gameObject);
+    }
+}
